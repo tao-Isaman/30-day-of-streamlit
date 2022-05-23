51eebb825e710ce8d0d2d7f38fdda84b8f800d4a
tao-isaman <towlovelove@gmail.com>
2022-05-23 20:55:54 +0700

    add streamlit day 10

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-from day_pages import day8, day9, default_page
+from day_pages import day8, day9, day10, default_page
 
 st.title('isaman.streamlit')
 st.header('30 day of Streamlit')
@@ -13,6 +13,7 @@ pages = {
     "Default" :  default_page.page,
     "Day 8" : day8.page,
     "Day 9" : day9.page,
+    "Day 10" : day10.page,
 }
 page = st.selectbox('Select Page',tuple(pages.keys()))
 
