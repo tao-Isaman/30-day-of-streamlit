7b43c64a0b5fefaa18e16f97029e94ebf27402d9
tao-isaman <towlovelove@gmail.com>
2022-06-05 09:57:45 +0700

    update day20

---
+++ day_pages/day20.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+def page():
+    
+    st.title('Tech Twitter Space on What is Streamlit?')
+    st.subheader('(Hosted by Francesco Ciulla)')
+    st.image('https://apps.streamlitusercontent.com/streamlit/30days/master/streamlit_app.py/+/media/7f614d069e6154af346505d4547593c3861650c7af56706fe5990116.jpeg')
+
+
+    st.markdown('👉 Link: https://twitter.com/i/spaces/1dRJZlbglXMKB')
